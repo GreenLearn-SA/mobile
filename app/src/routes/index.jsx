36ede366b747a0e2e3aed67bbcce91bf426fcef0
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-09-18 12:11:31 -0300

    routes

---
+++ app/src/routes/index.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../pages/login';
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
