2f6290c6d9b32ec80dc1e8482fcc011d42ffa0b6
Pedro Nieto <pedronieto.2005@gmail.com>
2023-11-04 00:28:56 -0300

    wip: add user profile edition via patch

--- app/src/pages/User/User.jsx
+++ app/src/pages/User/User.jsx
@@ -1,7 +1,9 @@
 import { ScrollView, StyleSheet, View, Alert, Image } from "react-native";
 import { Appbar, Text, Avatar, TextInput, IconButton, Modal, Provider as PaperProvider, DefaultTheme, Button } from 'react-native-paper';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import mind from './img/mind.png'
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const theme = {
     ...DefaultTheme,
@@ -11,18 +13,89 @@ const theme = {
     },
 };
 
-export default function User({ navigation, route }) {
-    const { firstName, lastName } = route.params;
+export default function User({ navigation }) {
+    const [passwordVisible, setPasswordVisible] = useState(true);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const accessToken = await AsyncStorage.getItem('accessToken');
+                if (!accessToken) {
+                    console.error('Access token is missing.');
+                    return;
+                }
+
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    },
+                };
+
+                axios.get('http://10.0.0.103:3000/auth/profile', config)
+                    .then((response) => {
+                        setFirstName(response.data.firstName);
+                        setLastName(response.data.lastName);
+                        setUsername(response.data.username);
+                    })
+                    .catch((error) => {
+                        console.error("Erro ao puxar dados", error);
+                    });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const [newFirstName, setNewFirstName] = useState('');
+    const [newLastName, setNewLastName] = useState('');
+    const [newUsername, setNewUsername] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [newPassword, setNewPassword] = useState('');
     const avatarLabel = firstName.charAt(0) + lastName.charAt(0);
 
-    const [passwordVisible, setPasswordVisible] = useState(true);
-    const [newFirstName, setNewFirstName] = useState(firstName);
-    const [newLastName, setNewLastName] = useState(lastName);
-    const [email, setEmail] = useState("ananegri@proflinda.com");
-    const [username, setUsername] = useState("AnaNegriDogLover");
-    const [password, setPassword] = useState("amoDogs3C");
+    async function saveChanges() {
+        try {
+            const accessToken = await AsyncStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is missing.');
+                return;
+            }
 
-    //*Modal
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+            };
+
+            const updatedData = {
+                firstName: newFirstName,
+                lastName: newLastName,
+                username: newUsername,
+                email: newEmail,
+                password: newPassword,
+                isManager: false
+            };
+
+            axios.patch(`http://10.0.0.103:3000/user/update/${username}`, updatedData, config)
+                .then((response) => {
+                    console.warn(response);
+                })
+                .catch((error) => {
+                    console.error("Erro ao atualizar informações do usuário", error);
+                    console.info(username);
+                    console.info(newUsername);
+                });
+        } catch (error) {
+            console.error(error);
+        }
+
+
+    };
+
     const [visible, setVisible] = React.useState(false);
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
@@ -35,7 +108,11 @@ export default function User({ navigation, route }) {
             [
                 {
                     text: 'Sim',
-                    onPress: () => Alert.alert('Informações atualizadas com sucesso'),
+                    onPress: () => {
+                        setFirstName(newFirstName);
+                        setLastName(newLastName);
+                        saveChanges();
+                    },
                     style: 'default',
                 },
                 {
@@ -66,6 +143,7 @@ export default function User({ navigation, route }) {
                 </View>
 
                 <View>
+                    <Text style={styles.editInfos}>Altere suas informações abaixo</Text>
                     <TextInput
                         mode='outlined'
                         cancelable='true'
@@ -74,7 +152,7 @@ export default function User({ navigation, route }) {
                         value={newFirstName}
                         outlineColor='#71a42a'
                         selectionColor='#71a42a'
-                        onChangeText={text => setUsername(text)}
+                        onChangeText={text => setNewFirstName(text)}
                         left={
                             <TextInput.Icon
                                 icon={'account'}
@@ -90,7 +168,7 @@ export default function User({ navigation, route }) {
                         value={newLastName}
                         outlineColor='#71a42a'
                         selectionColor='#71a42a'
-                        onChangeText={text => setUsername(text)}
+                        onChangeText={text => setNewLastName(text)}
                         left={
                             <TextInput.Icon
                                 icon={'account'}
@@ -103,10 +181,10 @@ export default function User({ navigation, route }) {
                         cancelable='true'
                         style={styles.input}
                         label={"Username"}
-                        value={username}
+                        value={newUsername}
                         outlineColor='#71a42a'
                         selectionColor='#71a42a'
-                        onChangeText={text => setUsername(text)}
+                        onChangeText={text => setNewUsername(text)}
                         left={
                             <TextInput.Icon
                                 icon={'account'}
@@ -119,10 +197,10 @@ export default function User({ navigation, route }) {
                         cancelable='true'
                         style={styles.input}
                         label={"E-mail"}
-                        value={email}
+                        value={newEmail}
                         outlineColor='#71a42a'
                         selectionColor='#71a42a'
-                        onChangeText={text => setUsername(text)}
+                        onChangeText={text => setNewEmail(text)}
                         left={
                             <TextInput.Icon
                                 icon={'email'}
@@ -135,11 +213,11 @@ export default function User({ navigation, route }) {
                         cancelable='true'
                         style={styles.input}
                         label={"Senha"}
-                        value={password}
+                        value={newPassword}
                         secureTextEntry={passwordVisible}
                         outlineColor='#71a42a'
                         selectionColor='#71a42a'
-                        onChangeText={text => setPassword(text)}
+                        onChangeText={text => setNewPassword(text)}
                         right={
                             <TextInput.Icon
                                 icon={passwordVisible ? 'eye-off' : 'eye'}
@@ -204,7 +282,12 @@ const styles = StyleSheet.create({
     greeting: {
         fontSize: 30,
         textAlign: 'center',
-
+    },
+    editInfos: {
+        marginTop: 20,
+        marginBottom: -10,
+        fontSize: 15,
+        textAlign: 'center',
     },
     input: {
         marginHorizontal: 50,
