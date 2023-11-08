53236fe11b7996958463c97dd736bd95b9c08b07
ashlyn <102593108+cemeterydriiver@users.noreply.github.com>
2023-11-08 12:02:27 -0300

    navbar calendar

--- app/src/pages/login/index.jsx
+++ app/src/pages/login/index.jsx
@@ -27,7 +27,7 @@ export default function Login({ navigation }) {
       password: password,
     }
 
-    axios.post('http://10.0.0.103:3000/auth/login', userSignInData)
+    axios.post('http://10.3.118.200:3000/auth/login', userSignInData)
       .then((signInSuccessResponse) => {
         AsyncStorage.setItem('accessToken', signInSuccessResponse.data);
         navigation.navigate('Main');
